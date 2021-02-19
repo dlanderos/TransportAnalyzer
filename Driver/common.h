0acbe66d0156019dc08753d45055916c7a216dae
dxboats <72629344+dxboats@users.noreply.github.com>
2021-02-19 18:40:19 -0500

    Add common header file

---
+++ Driver/common.h
@@ -0,0 +1,38 @@
+/*
+ * TransportAnalyzer: a simple WFP KMD for analyzing incoming and outgoing TCP packets.
+ * Copyright (C) 2021 dxboats
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+
+#ifndef TA_COMMON_H
+#define TA_COMMON_H
+
+
+
+#include <guiddef.h>
+#include <initguid.h>
+#include <devguid.h>
+#include <ntifs.h>
+#include <ntddk.h>
+#include <wdf.h>
+#include <fwpsk.h>
+#include <fwpmk.h>
+#include <fwpvi.h>
+
+
+
+#endif // !TA_COMMON_H

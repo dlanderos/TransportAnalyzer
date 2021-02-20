0ecf05790968075a9cc47a393c8c4aeb8dcffccd
dxboats <72629344+dxboats@users.noreply.github.com>
2021-02-19 20:44:29 -0500

    Add basic definition and implementation for callouts

---
+++ Driver/callouts.c
@@ -0,0 +1,196 @@
+#include "callouts.h"
+
+
+
+UINT32 gCalloutInboundIdentifier;
+UINT32 gCalloutOutboundIdentifier;
+
+
+
+/*
+ * Creates the inbound callout with the provided filter engine handle and device object pointer.
+ */
+NTSTATUS taCreateCalloutInbound(_In_ HANDLE engineHandle, _In_ PDEVICE_OBJECT pDeviceObject) {
+	NTSTATUS status = STATUS_SUCCESS;
+
+	FWPS_CALLOUT calloutRegister = {
+		.calloutKey = TA_CALLOUT_INBOUND_GUID,
+		.flags = 0,
+		.classifyFn = taClassifyInbound,
+		.notifyFn = taNotifyInbound,
+		.flowDeleteFn = taFlowDeleteInbound,
+	};
+
+	status = FwpsCalloutRegister(pDeviceObject, &calloutRegister, &gCalloutInboundIdentifier);
+	TA_DEBUG_ONLY_LOG_ERROR_STATUS("failed to register inbound callout", status);
+	if (!NT_SUCCESS(status)) {
+		return status;
+	}
+
+	FWPM_CALLOUT calloutAdd = {
+		.calloutKey = TA_CALLOUT_INBOUND_GUID,
+		.displayData = {
+			.name = TA_CALLOUT_INBOUND_NAME,
+			.description = TA_CALLOUT_INBOUND_DESCRIPTION,
+		},
+		.flags = 0,
+		.applicableLayer = FWPM_LAYER_INBOUND_TRANSPORT_V4,
+		.calloutId = gCalloutInboundIdentifier,
+	};
+
+	status = FwpmCalloutAdd(engineHandle, &calloutAdd, NULL, NULL);
+	TA_DEBUG_ONLY_LOG_ERROR_STATUS("failed to add inbound callout to filter engine", status);
+	return status;
+}
+
+
+
+/*
+ * Deletes the inbound callout with the provided filter engine handle and device object pointer.
+ * Unsure if the callout needs to be both unregistered and deleted?
+ */
+NTSTATUS taDeleteCalloutInbound(_In_ HANDLE engineHandle, _In_ PDEVICE_OBJECT pDeviceObject) {
+	UNREFERENCED_PARAMETER(engineHandle);
+	UNREFERENCED_PARAMETER(pDeviceObject);
+	NTSTATUS status = STATUS_SUCCESS;
+	status = FwpsCalloutUnregisterById(gCalloutInboundIdentifier);
+	TA_DEBUG_ONLY_LOG_ERROR_STATUS("failed to unregister inbound callout", status);
+	if (!NT_SUCCESS(status)) {
+		return status;
+	}
+	status = FwpmCalloutDeleteByKey(engineHandle, &TA_CALLOUT_INBOUND_GUID);
+	TA_DEBUG_ONLY_LOG_ERROR_STATUS("failed to delete inbound callout from filter engine", status);
+	return status;
+}
+
+
+
+/*
+ * Always allows the packet through. Will print the packet's payload and TCP header flags.
+ */
+VOID NTAPI taClassifyInbound(
+	_In_ const FWPS_INCOMING_VALUES* pInFixedValues,
+	_In_ const FWPS_INCOMING_METADATA_VALUES* pInMetaValues,
+	_Inout_opt_ void* pLayerData,
+	_In_opt_ const void* pClassifyContext,
+	_In_ const FWPS_FILTER* pFilter, _In_ UINT64 flowContext, _Inout_ FWPS_CLASSIFY_OUT* pClassifyOut
+) {
+
+}
+
+
+
+/*
+ * Doesn't do anything. There currently is no need to listen to callout events.
+ */
+NTSTATUS NTAPI taNotifyInbound(_In_ FWPS_CALLOUT_NOTIFY_TYPE notifyType, _In_ const GUID* pFilterKey, _Inout_ FWPS_FILTER* pFilter) {
+	UNREFERENCED_PARAMETER(notifyType);
+	UNREFERENCED_PARAMETER(pFilterKey);
+	UNREFERENCED_PARAMETER(pFilter);
+}
+
+
+
+/*
+ * Doesn't do anything. There currently aren't any resources that need to be cleaned up once the data is stopped.
+ */
+VOID NTAPI taFlowDeleteInbound(_In_ UINT16 layerId, _In_ UINT32 calloutId, _In_ UINT64 flowContext) {
+	UNREFERENCED_PARAMETER(layerId);
+	UNREFERENCED_PARAMETER(calloutId);
+	UNREFERENCED_PARAMETER(flowContext);
+}
+
+
+
+/*
+ * Creates the outbound callout with the provided filter engine handle and device object pointer.
+ */
+NTSTATUS taCreateCalloutOutbound(_In_ HANDLE engineHandle, _In_ PDEVICE_OBJECT pDeviceObject) {
+	NTSTATUS status = STATUS_SUCCESS;
+
+	FWPS_CALLOUT calloutRegister = {
+		.calloutKey = TA_CALLOUT_OUTBOUND_GUID,
+		.flags = 0,
+		.classifyFn = taClassifyOutbound,
+		.notifyFn = taNotifyOutbound,
+		.flowDeleteFn = taFlowDeleteOutbound,
+	};
+
+	status = FwpsCalloutRegister(pDeviceObject, &calloutRegister, &gCalloutOutboundIdentifier);
+	TA_DEBUG_ONLY_LOG_ERROR_STATUS("failed to register outbound callout", status);
+	if (!NT_SUCCESS(status)) {
+		return status;
+	}
+
+	FWPM_CALLOUT calloutAdd = {
+		.calloutKey = TA_CALLOUT_OUTBOUND_GUID,
+		.displayData = {
+			.name = TA_CALLOUT_OUTBOUND_NAME,
+			.description = TA_CALLOUT_OUTBOUND_DESCRIPTION,
+		},
+		.flags = 0,
+		.applicableLayer = FWPM_LAYER_OUTBOUND_TRANSPORT_V4,
+		.calloutId = gCalloutOutboundIdentifier,
+	};
+
+	status = FwpmCalloutAdd(engineHandle, &calloutAdd, NULL, NULL);
+	TA_DEBUG_ONLY_LOG_ERROR_STATUS("failed to add outbound callout to filter engine", status);
+	return status;
+}
+
+
+
+/*
+ * Deletes the outbound callout with the provided filter engine handle and device object pointer.
+ * Unsure if the callout needs to be both unregistered and deleted?
+ */
+NTSTATUS taDeleteCalloutOutbound(_In_ HANDLE engineHandle, _In_ PDEVICE_OBJECT pDeviceObject) {
+	UNREFERENCED_PARAMETER(engineHandle);
+	UNREFERENCED_PARAMETER(pDeviceObject);
+	NTSTATUS status = STATUS_SUCCESS;
+	status = FwpsCalloutUnregisterById(gCalloutOutboundIdentifier);
+	TA_DEBUG_ONLY_LOG_ERROR_STATUS("failed to unregister outbound callout", status);
+	if (!NT_SUCCESS(status)) {
+		return status;
+	}
+	status = FwpmCalloutDeleteByKey(engineHandle, &TA_CALLOUT_OUTBOUND_GUID);
+	TA_DEBUG_ONLY_LOG_ERROR_STATUS("failed to delete outbound callout from filter engine", status);
+	return status;
+}
+
+
+
+/*
+ * Always allows the packet through. Will print the packet's payload and TCP header flags.
+ */
+VOID NTAPI taClassifyOutbound(
+	_In_ const FWPS_INCOMING_VALUES* pInFixedValues,
+	_In_ const FWPS_INCOMING_METADATA_VALUES* pInMetaValues,
+	_Inout_opt_ void* pLayerData,
+	_In_opt_ const void* pClassifyContext,
+	_In_ const FWPS_FILTER* pFilter, _In_ UINT64 flowContext, _Inout_ FWPS_CLASSIFY_OUT* pClassifyOut
+) {
+
+}
+
+
+
+/*
+ * Doesn't do anything. There currently is no need to listen to callout events.
+ */
+NTSTATUS NTAPI taNotifyOutbound(_In_ FWPS_CALLOUT_NOTIFY_TYPE notifyType, _In_ const GUID* pFilterKey, _Inout_ FWPS_FILTER* pFilter) {
+	UNREFERENCED_PARAMETER(notifyType);
+	UNREFERENCED_PARAMETER(pFilterKey);
+	UNREFERENCED_PARAMETER(pFilter);
+}
+
+
+
+/*
+ * Doesn't do anything. There currently aren't any resources that need to be cleaned up once the data is stopped.
+ */
+VOID NTAPI taFlowDeleteOutbound(_In_ UINT16 layerId, _In_ UINT32 calloutId, _In_ UINT64 flowContext) {
+	UNREFERENCED_PARAMETER(layerId);
+	UNREFERENCED_PARAMETER(calloutId);
+	UNREFERENCED_PARAMETER(flowContext);
+}
---
+++ Driver/callouts.h
@@ -0,0 +1,133 @@
+#ifndef TA_CALLOUTS_H
+#define TA_CALLOUTS_H
+
+
+
+#include "common.h"
+#include "debug.h"
+
+
+
+// a0c5675d-8c23-462f-8353-1a913b6fc0af
+DEFINE_GUID(TA_CALLOUT_INBOUND_GUID, 0xa0c5675dL, 0x8c23, 0x462f, 0x83, 0x53, 0x1a, 0x91, 0x3b, 0x6f, 0xc0, 0xaf);
+#define TA_CALLOUT_INBOUND_NAME L"ta_callout_inbound"
+#define TA_CALLOUT_INBOUND_DESCRIPTION L"Callout used for inspecting inbound TCP packets"
+#define TA_CALLOUT_INBOUND_POOL_TAG (UINT32) 'IAT'
+
+
+
+// ee94ec3d-04d7-44d0-b765-3841adfa2f38
+DEFINE_GUID(TA_CALLOUT_OUTBOUND_GUID, 0xee94ec3dL, 0x04d7, 0x44d0, 0xb7, 0x65, 0x38, 0x41, 0xad, 0xfa, 0x2f, 0x38);
+#define TA_CALLOUT_OUTBOUND_NAME L"ta_callout_outbound"
+#define TA_CALLOUT_OUTBOUND_DESCRIPTION L"Callout used for inspecting outbound TCP packets"
+#define TA_CALLOUT_OUTBOUND_POOL_TAG (UINT32) 'OAT'
+
+
+
+/*
+ * Creates the inbound callout with the provided filter engine handle and device object pointer.
+ */
+NTSTATUS taCreateCalloutInbound(_In_ HANDLE engineHandle, _In_ PDEVICE_OBJECT pDeviceObject);
+
+
+
+/*
+ * Deletes the inbound callout with the provided filter engine handle and device object pointer.
+ */
+NTSTATUS taDeleteCalloutInbound(_In_ HANDLE engineHandle, _In_ PDEVICE_OBJECT pDeviceObject);
+
+
+
+/*
+ * Handles each classification action on the inbound side of the transport layer. This function will do the actual
+ * analyzation of the TCP packets. See <https://docs.microsoft.com/en-us/windows-hardware/drivers/network/processing-classify-callouts>
+ * for more information.
+ */
+VOID NTAPI taClassifyInbound(
+	_In_ const FWPS_INCOMING_VALUES* pInFixedValues,
+	_In_ const FWPS_INCOMING_METADATA_VALUES* pInMetaValues,
+	_Inout_opt_ void* pLayerData,
+	_In_opt_ const void* pClassifyContext,
+	_In_ const FWPS_FILTER* pFilter, _In_ UINT64 flowContext, _Inout_ FWPS_CLASSIFY_OUT* pClassifyOut
+);
+
+
+
+/*
+ * Handles events that are in association with the inbound callout.
+ * See <https://docs.microsoft.com/en-us/windows-hardware/drivers/network/processing-notify-callouts> for more information.
+ */
+NTSTATUS NTAPI taNotifyInbound(
+	_In_ FWPS_CALLOUT_NOTIFY_TYPE notifyType,
+	_In_ const GUID* pFilterKey,
+	_Inout_ FWPS_FILTER* pFilter
+);
+
+
+
+/*
+ * Handles cleanup when the data flow has been stopped on the inbound callout.
+ * See <https://docs.microsoft.com/en-us/windows-hardware/drivers/network/processing-flow-delete-callouts> for more information.
+ */
+VOID NTAPI taFlowDeleteInbound(
+	_In_ UINT16 layerId,
+	_In_ UINT32 calloutId,
+	_In_ UINT64 flowContext
+);
+
+
+
+/*
+ * Creates the outbound callout with the provided filter engine handle and device object pointer.
+ */
+NTSTATUS taCreateCalloutOutbound(_In_ HANDLE engineHandle, _In_ PDEVICE_OBJECT pDeviceObject);
+
+
+
+/*
+ * Deletes the outbound callout with the provided filter engine handle and device object pointer.
+ */
+NTSTATUS taDeleteCalloutOutbound(_In_ HANDLE engineHandle, _In_ PDEVICE_OBJECT pDeviceObject);
+
+
+
+/*
+ * Handles each classification action on the outbound side of the transport layer. This function will do the actual
+ * analyzation of the TCP packets. See <https://docs.microsoft.com/en-us/windows-hardware/drivers/network/processing-classify-callouts>
+ * for more information.
+ */
+VOID NTAPI taClassifyOutbound(
+	_In_ const FWPS_INCOMING_VALUES* pInFixedValues,
+	_In_ const FWPS_INCOMING_METADATA_VALUES* pInMetaValues,
+	_Inout_opt_ void* pLayerData,
+	_In_opt_ const void* pClassifyContext,
+	_In_ const FWPS_FILTER* pFilter, _In_ UINT64 flowContext, _Inout_ FWPS_CLASSIFY_OUT* pClassifyOut
+);
+
+
+
+/*
+ * Handles events that are in association with the outbound callout.
+ * See <https://docs.microsoft.com/en-us/windows-hardware/drivers/network/processing-notify-callouts> for more information.
+ */
+NTSTATUS NTAPI taNotifyOutbound(
+	_In_ FWPS_CALLOUT_NOTIFY_TYPE notifyType,
+	_In_ const GUID* pFilterKey,
+	_Inout_ FWPS_FILTER* pFilter
+);
+
+
+
+/*
+ * Handles cleanup when the data flow has been stopped on the outbound callout.
+ * See <https://docs.microsoft.com/en-us/windows-hardware/drivers/network/processing-flow-delete-callouts> for more information.
+ */
+VOID NTAPI taFlowDeleteOutbound(
+	_In_ UINT16 layerId,
+	_In_ UINT32 calloutId,
+	_In_ UINT64 flowContext
+);
+
+
+
+#endif // !TA_CALLOUTS_H

4d41c01d33b57cb9b5198d895418b708437de748
dxboats <72629344+dxboats@users.noreply.github.com>
2021-03-01 20:44:03 -0500

    Add basic implementation for outbound analyzation

--- Driver/callout_outbound.c
+++ Driver/callout_outbound.c
@@ -93,7 +93,107 @@ VOID NTAPI taClassifyOutbound(
 	_In_opt_ const void* pClassifyContext,
 	_In_ const FWPS_FILTER* pFilter, _In_ UINT64 flowContext, _Inout_ FWPS_CLASSIFY_OUT* pClassifyOut
 ) {
+	// Unreferenced parameters
+	UNREFERENCED_PARAMETER(pClassifyContext);
+	UNREFERENCED_PARAMETER(pFilter);
+	UNREFERENCED_PARAMETER(flowContext);
 
+	// Track progression of callout processing
+	//BOOL							 bufferRetreat = FALSE;
+	BOOL							 bufferAllocated		 = FALSE;
+	ULONG							 transportHeaderSize	 = 0;
+	PNET_BUFFER_LIST				 pNetBufferList			 = NULL;
+	PNET_BUFFER						 pNetBuffer				 = NULL;
+	PBYTE							 pAllocatedBuffer		 = NULL;
+
+	// Ensure that the transport header size is specified within the meta values
+	if (!FWPS_IS_METADATA_FIELD_PRESENT(pInMetaValues, FWPS_METADATA_FIELD_TRANSPORT_HEADER_SIZE)) {
+		goto finalize;
+	}
+
+	// Ensure that there is layer data
+	if (!pLayerData) {
+		goto finalize;
+	}
+
+	// Get the local port and address
+	UINT16 localPort = pInFixedValues->incomingValue[FWPS_FIELD_OUTBOUND_TRANSPORT_V4_IP_LOCAL_PORT].value.uint16;
+	UINT32 localAddress = pInFixedValues->incomingValue[FWPS_FIELD_OUTBOUND_TRANSPORT_V4_IP_LOCAL_ADDRESS].value.uint32;
+
+	// Get the remote port and address
+	UINT16 remotePort = pInFixedValues->incomingValue[FWPS_FIELD_OUTBOUND_TRANSPORT_V4_IP_REMOTE_PORT].value.uint16;
+	UINT32 remoteAddress = pInFixedValues->incomingValue[FWPS_FIELD_OUTBOUND_TRANSPORT_V4_IP_REMOTE_ADDRESS].value.uint32;
+
+	// Retrieve the transport header size meta value
+	transportHeaderSize = pInMetaValues->transportHeaderSize;
+
+	// Get the first net buffer
+	pNetBufferList = (PNET_BUFFER_LIST) pLayerData;
+	pNetBuffer = NET_BUFFER_LIST_FIRST_NB(pNetBufferList);
+
+	// Get the length of the packet's data AND TCP header
+	// Notice, that the length includes the header this time
+	ULONG packetLength = NET_BUFFER_DATA_LENGTH(pNetBuffer);
+
+	// Attempt to allocate a buffer for the TCP header and data
+	// In the real world, you probably would want to allocate up to the TCP header, read its contents, then
+	// make a call to NdisAdvanceNetBufferDataStart to start right at the data section of the packet.
+	// Don't forget to retreat the buffer back when you're done if you decide to go this route!
+	pAllocatedBuffer = (PBYTE) ExAllocatePoolZero(NonPagedPoolNx, (size_t) packetLength, TA_CALLOUT_INBOUND_POOL_TAG);
+	if (!pAllocatedBuffer) {
+		goto finalize;
+	}
+	bufferAllocated = TRUE;
+
+	// Attempt to get a contiguous buffer from which the TCP header and data is actually read from
+	PBYTE pContiguousBuffer = (PBYTE) NdisGetDataBuffer(pNetBuffer, (ULONG) packetLength, pAllocatedBuffer, 1, 0);
+	if (!pContiguousBuffer) {
+		goto finalize;
+	}
+
+	// Get the TCP packet's checksum (I guess it's always zero when at this layer)
+	UINT16 checksum = taTCPGetChecksum(pContiguousBuffer);
+
+	// Indicate the beginning of a packet
+	DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "BEGIN PACKET: %04x\n", checksum);
+
+	// Indicate direction of packet
+	DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL,
+		"Direction: %d.%d.%d.%d:%d -> %d.%d.%d.%d:%d",
+		PRETTY_ADDRESS(localAddress), localPort,
+		PRETTY_ADDRESS(remoteAddress), remotePort
+	);
+
+	// Print the TCP Flags
+	taTCPPrintFlags(pContiguousBuffer);
+
+	// Print data if there is some
+	if (packetLength != transportHeaderSize) {
+		//DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "Data:\n");
+
+		// Print each byte of data (in little-endian)
+		for (ULONG index = 0; index < packetLength; index++) {
+			BYTE value = *(transportHeaderSize + pContiguousBuffer + index);
+			CHAR byteString[9];
+			taTCPPrettyByte(value, byteString);
+			DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "(%04d) (0x%02x) %s\n", value, value, byteString);
+		}
+	}
+
+	// Indicate the ending of the packet
+	DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "END PACKET: %04x\n", checksum);
+
+	// DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "TransportHeaderSize: %d, DataLength: %d, PacketLength: %d", transportHeaderSize, dataLength, packetLength);
+
+finalize:
+	// Ensure that the buffer is freed
+	if (bufferAllocated) {
+		ExFreePoolWithTag((PVOID) pAllocatedBuffer, TA_CALLOUT_INBOUND_POOL_TAG);
+		pAllocatedBuffer = 0;
+	}
+
+	// Default action is to permit
+	pClassifyOut->actionType = FWP_ACTION_PERMIT;
 }
 
 
@@ -105,6 +205,7 @@ NTSTATUS NTAPI taNotifyOutbound(_In_ FWPS_CALLOUT_NOTIFY_TYPE notifyType, _In_ c
 	UNREFERENCED_PARAMETER(notifyType);
 	UNREFERENCED_PARAMETER(pFilterKey);
 	UNREFERENCED_PARAMETER(pFilter);
+	return STATUS_SUCCESS;
 }
 
 
